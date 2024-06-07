0ffeaf3712ad19e19f6d4aa6b34cea82f35da9fb
mirna-k <mirna_krpic@hotmail.com>
2024-06-07 14:05:20 +0200

    Add app

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from PIL import Image
+
+st.title("Klasifikacija rijetkih biljaka i životinja")
+
+classification_method = st.selectbox(
+    'Odaberite model za klasifikaciju',
+    ('Swin_V2 - najbolji za klasifikaciju životinja', 'ResNet50 - najbolji za klasifikaciju biljaka', 'MyFSL')
+)
+
+uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+if st.button('Process Image'):
+    if uploaded_image is not None:
+        image = Image.open(uploaded_image)
+
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+        
+        st.text("Processing the image...")
+
+        result = f"Predpostavljena vrsta je Vicugna vicugna\nNaziv: Vikuna\nVikuna je vrsta južnoameričke deve koja živi u Andama."
+
+        st.text(result)
+    else:
+        st.text("Please upload an image before clicking the button.")
